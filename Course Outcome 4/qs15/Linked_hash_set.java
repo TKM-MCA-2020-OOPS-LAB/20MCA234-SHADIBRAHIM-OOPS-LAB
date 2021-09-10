db5f26064ab8f3d2fec0ee84c6015211ac22e8cb
SHADIBRAHIM <74901200+SHADIBRAHIM@users.noreply.github.com>
2021-09-10 23:24:52 +0530

    Add files via upload

---
+++ Course Outcome 4/qs15/Linked_hash_set.java
@@ -0,0 +1,19 @@
+import java.util.LinkedHashSet;
+public class Linked_hash_set {
+    public static void main(String[] args) {
+		LinkedHashSet<String> LHS = new LinkedHashSet<String>();
+		LHS.add("Shad");
+		LHS.add("Vasim");
+		LHS.add("Fahad");
+		LHS.add("Adharsh");
+		LHS.add("Rashi");
+		LHS.add("Asshu");
+		LHS.add("Shameem");
+		LHS.add("Hudaif");
+		System.out.println("Size of LinkedHashSet = " +LHS.size());
+		System.out.println("The LinkedHashSet is"+LHS);
+
+}
+}
+
+    
